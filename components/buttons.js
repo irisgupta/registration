60f8330603b3c77876a56922afdf5d8fec094cb1
Iris Gupta <iris.gupta0@gmail.com>
2022-08-01 18:52:37 -0400

    saved button value

--- components/buttons.js
+++ components/buttons.js
@@ -1,30 +1,45 @@
 import styles from '../styles/experiment.module.css'
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
 export default function Buttons() {
-    const storeButtonValue = (e) => {
-        e.preventDefault();
-        return(e.currentTarget.id);
-      }
+    
+    const confidenceScore = 0;
 
-    return (
-        
+    const buttonOne = () => { 
+      confidenceScore=1;
+    }
+    const buttonTwo = () => { 
+      confidenceScore=2;
+    }
+    const buttonThree = () => { 
+      confidenceScore=3;
+    }
+    const buttonFour = () => { 
+      confidenceScore=4;
+    }
+    const buttonFive = () => { 
+      confidenceScore=5;
+    }
+
+    return (   
+
       <div>
+
         <div className={styles.alignButtons}>
           <div className={styles.buttonContainer}>
-            <button className={styles.buttons} id="one" onClick={storeButtonValue}></button>
+            <button className={styles.buttons} onClick={buttonOne}></button>
           </div>
           <div className={styles.buttonContainer}>
-            <button className={styles.buttons} id="two" onClick={storeButtonValue}></button>
+            <button className={styles.buttons} onClick={buttonTwo}></button>
           </div>
           <div className={styles.buttonContainer}>
-            <button className={styles.buttons} id="three" onClick={storeButtonValue}></button>
+            <button className={styles.buttons} onClick={buttonThree}></button>
           </div>
           <div className={styles.buttonContainer}>
-            <button className={styles.buttons} id="four" onClick={storeButtonValue}></button>
+            <button className={styles.buttons} onClick={buttonFour}></button>
           </div>
           <div className={styles.buttonContainer}>
-            <button className={styles.buttons} id="five" onClick={storeButtonValue}></button>
+            <button className={styles.buttons} onClick={buttonFive}></button>
           </div>
         </div>
         <ul>
